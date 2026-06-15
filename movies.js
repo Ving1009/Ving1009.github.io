65033e528b6f6c3b555f76a22e5919b14e33e321
Ving1009 <le10zinh@gmail.com>
2026-06-15 23:24:53 +0700

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,117 +1,68 @@
 window.MOVIE_DATA = [
-  {
+ {
     id: "pokemonxyz",
     title: "POKEMON XYZ",
     thumbnail: "https://m.media-amazon.com/images/M/MV5BOWU5MDI0NjMtMWRjMy00NjNmLWI1MjctYzRmMDQ2MDFmZTM1XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
-    description: "Một nhóm bạn phát hiện tín hiệu lạ từ trạm phát cũ và lần theo bí mật bị che giấu nhiều năm.",
+    description: "Hành trình đầy thử thách của Ash và các bạn tại vùng đất Kalos, đối đầu với Băng Ánh Lửa và khám phá bí ẩn về Zygarde.",
     category: "Phim bộ",
-    genres: ["Khoa học viễn tưởng", "Bí ẩn"],
+    genres: ["Hoạt hình", "Phiêu lưu", "Hành động"],
     year: 2026,
     isNew: true,
     seasons: [
       {
         name: "Mùa 19",
         episodes: [
-          {
-            id: "s1e1",
-            title: "Tập 1",
-            videoUrl: "https://drive.google.com/file/d/1EZyJ0vueU4UWLTFt0I0i_-8zuYOLbL93/preview"
-          },
-          {
-            id: "s1e2",
-            title: "Tập 2",
-            videoUrl: "https://drive.google.com/file/d/16lgXoUI8KIqktXhFxFhFiQP6Hub1fNqu/preview"
-          },
-          {
-            id: "s1e3",
-            title: "Tập 3",
-            videoUrl: "https://drive.google.com/file/d/14hUwEH_HpXrjxcgZyu8EWCKXRRWhv23z/preview"
-          },
-          {
-            id: "s1e4",
-            title: "Tập 4",
-            videoUrl: "https://drive.google.com/file/d/1D7l54S3FiBS3VrjOK4oHhGvcbXx4Sz3J/preview"
-          },
-          {
-            id: "s1e5",
-            title: "Tập 5",
-            videoUrl: "https://drive.google.com/file/d/1_cvSD_J9b7qn_9-Qb8fq9oHh75eL9_CQ/preview"
-          },
-          {
-            id: "s1e6",
-            title: "Tập 6",
-            videoUrl: "https://drive.google.com/file/d/1go0EtyaSB0I3Ru1aSH08zyOiNydeWiVo/preview"
-          },
-          {
-            id: "s1e7",
-            title: "Tập 7",
-            videoUrl: "https://drive.google.com/file/d/1xaVkTUvQPfUUQpYngu5PfeK_4MCvSV_r/preview"
-          },
-          {
-            id: "s1e8",
-            title: "Tập 8",
-            videoUrl: "https://drive.google.com/file/d/1kg_HE7enXTWPpOjGLpa2GM4GpGiNaff9/preview"
-          },
-          {
-            id: "s1e9",
-            title: "Tập 9",
-            videoUrl: "https://drive.google.com/file/d/1dMP3sXSjLeY42O_9-4Tgoip8D0TAx4GF/preview"
-          },
-          {
-            id: "s1e10",
-            title: "Tập 10",
-            videoUrl: "https://drive.google.com/file/d/1900xp5wW7vBswFRxQImzGp6nL3X3lCdo/preview"
-          },
-          {
-            id: "s1e11",
-            title: "Tập 11",
-            videoUrl: "https://drive.google.com/file/d/1Sa4-fVDiN5n7Lp2M4eiV26Z5MFxFN3n6/preview"
-          },
-          {
-            id: "s1e12",
-            title: "Tập 12",
-            videoUrl: "https://drive.google.com/file/d/1ZlbiODVdQjnkigK9U5L67fD4znxZmeOm/preview"
-          },
-          {
-            id: "s1e13",
-            title: "Tập 13",
-            videoUrl: "https://drive.google.com/file/d/1xNY8a9folmh6X3UzwcywwlSfy9tr-rvY/preview"
-          },
-          {
-            id: "s1e14",
-            title: "Tập 14",
-            videoUrl: "https://drive.google.com/file/d/1TJtp_0TrTgNOfrK84ITg29oW7Uu2FNrz/preview"
-          },
-          {
-            id: "s1e15",
-            title: "Tập 15",
-            videoUrl: "https://drive.google.com/file/d/1qJo62jGW2vh5QfB4LS2Ww2wEnPQBMKNr/preview"
-          },
-          {
-            id: "s1e16",
-            title: "Tập 16",
-            videoUrl: "https://drive.google.com/file/d/1eEcPX-w-S70bZhbSc77fX26OOPdX9sl4/preview"
-          },
-          {
-            id: "s1e17",
-            title: "Tập 17",
-            videoUrl: "https://drive.google.com/file/d/1zRW5QEVOVc--QfDMAjjJdcKf0kFiwVRf/preview"
-          },
-          {
-            id: "s1e18",
-            title: "Tập 18",
-            videoUrl: "https://drive.google.com/file/d/1MJl2zBhcnfZ4N-hQScsniE-lECt25AgW/preview"
-          },
-          {
-            id: "s1e19",
-            title: "Tập 19",
-            videoUrl: "https://drive.google.com/file/d/1qECHZqUitu9kMJIcRCtlm6Loso2uTFuS/preview"
-          },
-          {
-            id: "s1e20",
-            title: "Tập 20",
-            videoUrl: "https://drive.google.com/file/d/1esIFqZA99AqSmcDWl6iKAeRoJk62o-z0/preview"
-          }
+          { id: "s1e1", title: "Tập 1", videoUrl: "https://drive.google.com/file/d/1EZyJ0vueU4UWLTFt0I0i_-8zuYOLbL93/preview" },
+          { id: "s1e2", title: "Tập 2", videoUrl: "https://drive.google.com/file/d/16lgXoUI8KIqktXhFxFhFiQP6Hub1fNqu/preview" },
+          { id: "s1e3", title: "Tập 3", videoUrl: "https://drive.google.com/file/d/14hUwEH_HpXrjxcgZyu8EWCKXRRWhv23z/preview" },
+          { id: "s1e4", title: "Tập 4", videoUrl: "https://drive.google.com/file/d/1D7l54S3FiBS3VrjOK4oHhGvcbXx4Sz3J/preview" },
+          { id: "s1e5", title: "Tập 5", videoUrl: "https://drive.google.com/file/d/1_cvSD_J9b7qn_9-Qb8fq9oHh75eL9_CQ/preview" },
+          /* Thay link mới cho tập 6 */
+          { id: "s1e6", title: "Tập 6", videoUrl: "https://drive.google.com/file/d/1nih9ESWYwXYan9X-o9kg1d3ZO4XEjaIj/preview" },
+          /* Tạm giữ các tập từ 7 đến 20 */
+          { id: "s1e7", title: "Tập 7", videoUrl: "https://drive.google.com/file/d/1xaVkTUvQPfUUQpYngu5PfeK_4MCvSV_r/preview" },
+          { id: "s1e8", title: "Tập 8", videoUrl: "https://drive.google.com/file/d/1kg_HE7enXTWPpOjGLpa2GM4GpGiNaff9/preview" },
+          { id: "s1e9", title: "Tập 9", videoUrl: "https://drive.google.com/file/d/1dMP3sXSjLeY42O_9-4Tgoip8D0TAx4GF/preview" },
+          { id: "s1e10", title: "Tập 10", videoUrl: "https://drive.google.com/file/d/1900xp5wW7vBswFRxQImzGp6nL3X3lCdo/preview" },
+          { id: "s1e11", title: "Tập 11", videoUrl: "https://drive.google.com/file/d/1Sa4-fVDiN5n7Lp2M4eiV26Z5MFxFN3n6/preview" },
+          { id: "s1e12", title: "Tập 12", videoUrl: "https://drive.google.com/file/d/1ZlbiODVdQjnkigK9U5L67fD4znxZmeOm/preview" },
+          { id: "s1e13", title: "Tập 13", videoUrl: "https://drive.google.com/file/d/1xNY8a9folmh6X3UzwcywwlSfy9tr-rvY/preview" },
+          { id: "s1e14", title: "Tập 14", videoUrl: "https://drive.google.com/file/d/1TJtp_0TrTgNOfrK84ITg29oW7Uu2FNrz/preview" },
+          { id: "s1e15", title: "Tập 15", videoUrl: "https://drive.google.com/file/d/1qJo62jGW2vh5QfB4LS2Ww2wEnPQBMKNr/preview" },
+          { id: "s1e16", title: "Tập 16", videoUrl: "https://drive.google.com/file/d/1eEcPX-w-S70bZhbSc77fX26OOPdX9sl4/preview" },
+          { id: "s1e17", title: "Tập 17", videoUrl: "https://drive.google.com/file/d/1zRW5QEVOVc--QfDMAjjJdcKf0kFiwVRf/preview" },
+          { id: "s1e18", title: "Tập 18", videoUrl: "https://drive.google.com/file/d/1MJl2zBhcnfZ4N-hQScsniE-lECt25AgW/preview" },
+          { id: "s1e19", title: "Tập 19", videoUrl: "https://drive.google.com/file/d/1qECHZqUitu9kMJIcRCtlm6Loso2uTFuS/preview" },
+          { id: "s1e20", title: "Tập 20", videoUrl: "https://drive.google.com/file/d/1esIFqZA99AqSmcDWl6iKAeRoJk62o-z0/preview" },
+          /* Thêm loạt link mới từ tập 21 đến tập 48 */
+          { id: "s1e21", title: "Tập 21", videoUrl: "https://drive.google.com/file/d/1bxBcOkgnynQ2-yseCWW0EsyAVcVg-Ecr/preview" },
+          { id: "s1e22", title: "Tập 22", videoUrl: "https://drive.google.com/file/d/1113oCXyoWYmn9k0oZ0lpAnk_bwPI_k8p/preview" },
+          { id: "s1e23", title: "Tập 23", videoUrl: "https://drive.google.com/file/d/1jnUjNvVVrl_NVwG22bP7jFnl75eKlHjp/preview" },
+          { id: "s1e24", title: "Tập 24", videoUrl: "https://drive.google.com/file/d/1ouO8H0seoXquIb-4axy_pox2in4fFgYq/preview" },
+          { id: "s1e25", title: "Tập 25", videoUrl: "https://drive.google.com/file/d/1wtBUfxrxB4XVW5_mLNk1F6RF2EIQ5SrB/preview" },
+          { id: "s1e26", title: "Tập 26", videoUrl: "https://drive.google.com/file/d/1bJ-1YD-ADW569FFn3cCKKLSqaW2Dp-Q6/preview" },
+          { id: "s1e27", title: "Tập 27", videoUrl: "https://drive.google.com/file/d/1ceD3NEt8agvsKKwHSIBrlskb4xNEtY5j/preview" },
+          { id: "s1e28", title: "Tập 28", videoUrl: "https://drive.google.com/file/d/1cDvcWIf9ga9QFBbB_xBof1j-05IcdwWM/preview" },
+          { id: "s1e29", title: "Tập 29", videoUrl: "https://drive.google.com/file/d/1hlfO1IlxYl-nP01oqo0hsNhpq04m1nee/preview" },
+          { id: "s1e30", title: "Tập 30", videoUrl: "https://drive.google.com/file/d/16q_KCLBSBzjmJNZe1H1g0BGrYAgr8xAL/preview" },
+          { id: "s1e31", title: "Tập 31", videoUrl: "https://drive.google.com/file/d/1aZAHWPlOJW_7Lhuext-189RRp_BeKVpW/preview" },
+          { id: "s1e32", title: "Tập 32", videoUrl: "https://drive.google.com/file/d/1z54nSAnpk_vx3pqtegqS9v78PQTp0ndO/preview" },
+          { id: "s1e33", title: "Tập 33", videoUrl: "https://drive.google.com/file/d/1FnUfs-S3InEABFQpVrAQZabBvLyahkj7/preview" },
+          { id: "s1e34", title: "Tập 34", videoUrl: "https://drive.google.com/file/d/13M1Byga5s1uzwLkJQe-oZB5ru6_iMSYv/preview" },
+          { id: "s1e35", title: "Tập 35", videoUrl: "https://drive.google.com/file/d/1Qqjmf1rmNvCJ53lllAZhcAaEzRTOISQH/preview" },
+          { id: "s1e36", title: "Tập 36", videoUrl: "https://drive.google.com/file/d/1H6OXmSrcp-GYpO44ApJRsSxv9GfCenwy/preview" },
+          { id: "s1e37", title: "Tập 37", videoUrl: "https://drive.google.com/file/d/1Qa42LfVhWl58Zc4sEGGf-I5Rz6tXKWUk/preview" },
+          { id: "s1e38", title: "Tập 38", videoUrl: "https://drive.google.com/file/d/16fX9Dq5vCcY5iM3Bc2hNOSvwiqtieTtc/preview" },
+          { id: "s1e39", title: "Tập 39", videoUrl: "https://drive.google.com/file/d/13oPFOqdfl9xfhDsMZ3X4kc1ifADczO93/preview" },
+          { id: "s1e40", title: "Tập 40", videoUrl: "https://drive.google.com/file/d/15a_wqyHswQ-wQeAewZgzAeTmXGi2dP9m/preview" },
+          { id: "s1e41", title: "Tập 41", videoUrl: "https://drive.google.com/file/d/13xMsiwoTlgpW8xvMOW7EgZbFpn2S6SKV/preview" },
+          { id: "s1e42", title: "Tập 42", videoUrl: "https://drive.google.com/file/d/1i-ohQq0_TW6iAsioAiDP3rvst66VJcon/preview" },
+          { id: "s1e43", title: "Tập 43", videoUrl: "https://drive.google.com/file/d/1BK6G25ukF_TdkC3fq4HCgS6zGUjvfgKD/preview" },
+          { id: "s1e44", title: "Tập 44", videoUrl: "https://drive.google.com/file/d/1mqHxFgAkJ2gSybQQTOSrhYu8NpEfs_7K/preview" },
+          { id: "s1e45", title: "Tập 45", videoUrl: "https://drive.google.com/file/d/1R3kQb_Wx9IgddtzdnMcIeU9fZKl2xjgu/preview" },
+          { id: "s1e46", title: "Tập 46", videoUrl: "https://drive.google.com/file/d/14GIPCQ_vU0jNeacRcGH7rNT8Bbu7aM60/preview" },
+          { id: "s1e47", title: "Tập 47", videoUrl: "https://drive.google.com/file/d/14agBYoM9A4HidKS74yw6VRCEXvUeZfHB/preview" },
+          { id: "s1e48", title: "Tập 48", videoUrl: "https://drive.google.com/file/d/1vm3nygRJs3HEzcW8BG5QIlO02aKVjdpd/preview" }
         ]
       }
     ]
