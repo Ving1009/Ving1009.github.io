05181765c516e7ee50284654aeddbeb6a99879f1
Ving1009 <le10zinh@gmail.com>
2026-06-16 16:53:53 +0700

    Update movies.js

--- movies.js
+++ movies.js
@@ -2,7 +2,7 @@ window.MOVIE_DATA = [
  {
     id: "pokemonxyz",
     title: "POKEMON XYZ",
-    thumbnail: "https://m.media-amazon.com/images/M/MV5BOWU5MDI0NjMtMWRjMy00NjNmLWI1MjctYzRmMDQ2MDFmZTM1XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    thumbnail: "https://static.wikia.nocookie.net/pokemon/images/9/9d/Pok%C3%A9mon_XY_%26_Z_poster.png/revision/latest?cb=20150915185031",
     description: "Satoshi đã có bảy huy hiệu trong tay, chỉ còn một huy hiệu nữa là có thể tham gia giải đấu Liên đoàn Kalos. Vì vậy, cậu ấy cùng những người bạn tiếp tục hành trình giành huy hiệu nhà thi đấu thứ tám. Nhóm bạn không hay biết rằng, họ sẽ bị cuốn vào một cuộc khủng hoảng chưa từng có, một ngọn lửa xanh lá khủng khiếp sẽ nhấn chìm toàn bộ Pokémon và con người đang sinh sống ở vùng Kalos.",
     category: ["Phim bộ", "Hoạt hình"],
     genres: ["Hoạt hình", "Phiêu lưu", "Hành động"],
@@ -70,7 +70,7 @@ window.MOVIE_DATA = [
   {
     id: "conan-movie-28",
     title: "Thám Tử Lừng Danh Conan Movie 28 Dư Ảnh Của Độc Nhãn",
-    thumbnail: "https://cdn2.fptshop.com.vn/unsafe/800x0/conan_movie_28_lich_chieu_1_2fab7c4c57.jpg",
+    thumbnail: "https://upload.wikimedia.org/wikipedia/vi/3/3c/Conan_2025.jpg",
     description: "Trên những ngọn núi tuyết của Nagano, một vụ án bí ẩn đã đưa Conan và các thám tử quay trở lại quá khứ. Thanh tra Yamato Kansuke - người đã bị thương nặng trong một trận tuyết lở nhiều năm trước - bất ngờ phải đối mặt với những ký ức đau thương của mình trong khi điều tra một vụ tấn công tại Đài quan sát Nobeyama. Cùng lúc đó, Mori Kogoro nhận được một cuộc gọi từ một đồng nghiệp cũ, tiết lộ mối liên hệ đáng ngờ giữa anh ta và vụ án đã bị lãng quên từ lâu. Sự xuất hiện của Morofushi Takaaki, cùng với những nhân vật chủ chốt như Amuro Tooru, Kazami và cảnh sát Tokyo, càng làm phức tạp thêm cuộc điều tra. Khi quá khứ và hiện tại đan xen, một bí ẩn rùng rợn dần dần được hé lộ - và ký ức của Kansuke nắm giữ chìa khóa cho mọi thứ.",
     category: ["Phim lẻ", "Hoạt hình"],
     genres: ["Trinh thám", "Hành động", "Giật gân"],
@@ -92,7 +92,7 @@ window.MOVIE_DATA = [
   {
     id: "doraemon-movie-44",
     title: "Doraemon: Nobita Và Cuộc Phiêu Lưu Vào Thế Giới Trong Tranh",
-    thumbnail: "https://drive.google.com/file/d/19D1o_hXZ2m0qac15UJPy664hYSok71fH/preview",
+    thumbnail: "https://upload.wikimedia.org/wikipedia/vi/5/5d/Doraemon_Movie_2025_Poster.jpg?_=20250318144151",
     description: "Thông qua món bảo bối mới của Doraemon, cả nhóm bạn bước thế giới trong một bức tranh nổi tiếng và bắt gặp cô bạn bí ẩn tên Claire. Với lời mời của Claire, cả nhóm cùng đến thăm vương quốc Artoria, nơi ẩn giấu một viên ngọc quý mang tên Artoria Blue đang ngủ yên. Trên hành trình tìm kiếm viên ngọc, nhóm bạn Doraemon phát hiện một truyền thuyết về sự hủy diệt của thế giới, mà truyền thuyết đó dường như đang sống dậy! Liệu cả nhóm có thể phá hủy lời nguyền này và bảo vệ cả thế giới?",
     category: ["Phim lẻ", "Hoạt hình"],
     genres: ["Khoa học viễn tưởng giả tưởng", "Phiêu lưu", "Hài hước"],
@@ -103,31 +103,75 @@ window.MOVIE_DATA = [
         name: "Movie 44",
         episodes: [
           {
-            id: "movie1",
+            id: "movie-44",
             title: "Doraemon: Nobita Và Cuộc Phiêu Lưu Vào Thế Giới Trong Tranh",
-            videoUrl: "https://ok.ru/videoembed/15355206961753"
+            videoUrl: "https://mega.nz/embed/aOAhjYqC#MVOo9yNwd_er4W1ptxgqhTynKoauA7-el7xVstHqL10"
+          }
+        ]
+      }
+    ]
+  },
+{
+    id: "conan-movie-22",
+    title: "Thám Tử Lừng Danh Conan Movie 22 Kẻ Hành Pháp Zero",
+    thumbnail: "https://upload.wikimedia.org/wikipedia/vi/0/00/%C3%81p_ph%C3%ADch_phim_Th%C3%A1m_t%E1%BB%AD_l%E1%BB%ABng_danh_Conan_th%E1%BB%A9_20.jpg?_=20220416231551",
+    description: "Conan Movie 22 xoay quanh nhân vật Tooru 'Zero' Amuro và nghi vấn cảnh sát trưởng Kuroda là một trong những thành phần của tổ chức áo đen. 'Edge of Ocean', một cơ sở mới của Vịnh Tokyo sẽ là nơi tổ chức Hội nghị Thượng đỉnh Tokyo. Hội nghị sẽ được tổ chức vào ngày 1/5 và có tới 22.000 cảnh sát được huy động, nhưng một vụ nổ bom cực lớn đột ngột xảy ra tại cơ sở siêu hoành tráng này! Tại nơi đó, vào lúc xảy ra vụ việc, lại nhìn thấy bóng dáng của Amuro Tooru thuộc tổ chức bí mật của Cảnh sát Quốc gia với bí danh là 'Zero' đang điều khiển các cảnh sát an ninh trên toàn quốc. Tại hiện trường, cảnh sát đã phát hiện dấu vân tay của Mori Kogoro và ông bị bắt. Để chứng minh ông Mori vô tội, Conan đã bắt tay vào điều tra nhưng liên tục bị 'kẻ 3 mặt' Amuro cản đường.",
+    category: ["Phim lẻ", "Hoạt hình"],
+    genres: ["Trinh thám", "Hành động", "Giật gân"],
+    year: 2018,
+    isNew: true,
+    seasons: [
+      {
+        name: "Movie 22",
+        episodes: [
+          {
+            id: "movie-22",
+            title: "Kẻ Hành Pháp Zero",
+            videoUrl: "https://drive.google.com/file/d/1M5fv7EAmbBudycG_H6zkG7zv0wdi4gOw/preview"
+          }
+        ]
+      }
+    ]
+  },
+{
+    id: "conan-movie-20",
+    title: "Thám tử lừng danh Conan: Cơn ác mộng đen tối",
+    thumbnail: "https://upload.wikimedia.org/wikipedia/vi/0/00/%C3%81p_ph%C3%ADch_phim_Th%C3%A1m_t%E1%BB%AD_l%E1%BB%ABng_danh_Conan_th%E1%BB%A9_20.jpg?_=20220416231551",
+    description: "Những vụ ám sát bí ẩn xảy ra khắp thế giới. Kẻ tình nghi là một điệp viên có thể truy cập những tài liệu tuyệt mật. Ở Nhật Bản, một điệp viên đột nhập vào trụ sở cảnh sát và truy cập vào những tài liệu mật liên quan đến các cơ sở của điệp viên trong tổ chức Áo Đen đến từ rất nhiều cơ quan tình báo. Bị phát hiện trước khi đánh cắp các tài liệu mật, tên điệp viên bị Rei Furuya phát hiện và trốn thoát. Biệt đội thám tử của Conan nhập cuộc điều tra vụ án liên quan đến Tổ Chức Áo đen.",
+    category: ["Phim lẻ", "Hoạt hình"],
+    genres: ["Trinh thám", "Hành động", "Giật gân"],
+    year: 2014,
+    isNew: true,
+    seasons: [
+      {
+        name: "Movie 20",
+        episodes: [
+          {
+            id: "movie-20",
+            title: "Thám tử lừng danh Conan: Cơn ác mộng đen tối",
+            videoUrl: "https://drive.google.com/file/d/15RE0zWotwJRmgMSEBDBdpiSMK8Lg2FSm/preview"
           }
         ]
       }
     ]
   },
   {
-    id: "stage-room",
-    title: "Phòng Sân Khấu",
-    thumbnail: "https://images.unsplash.com/photo-1503095396549-807759245b35?auto=format&fit=crop&w=900&q=80",
-    description: "TV show phỏng vấn nghệ sĩ, hậu trường biểu diễn và những câu chuyện nghề nghiệp.",
-    category: "TV Show",
-    genres: ["Talkshow", "Âm nhạc"],
-    year: 2026,
+    id: "doraemon-movie-41",
+    title: "Nobita Và Cuộc Chiến Vũ Trụ Tí Hon",
+    thumbnail: "https://upload.wikimedia.org/wikipedia/vi/0/0e/Little_Star_Wars_2021.jpg",
+    description: "Tung Tung Tung Sahur",
+    category: ["Phim lẻ", "Hoạt hình"],
+    genres: ["Khoa học viễn tưởng giả tưởng", "Phiêu lưu", "Hài hước"],
+    year: 2021,
     isNew: true,
     seasons: [
       {
-        name: "Mùa 1",
+        name: "Movie 41",
         episodes: [
           {
-            id: "s1e1",
-            title: "Tập 1 - Đêm mở màn",
-            videoUrl: ""
+            id: "movie-41",
+            title: "Nobita Và Cuộc Chiến Vũ Trụ Tí Hon",
+            videoUrl: "https://drive.google.com/file/d/1_jpweXb1C_4XzIioqkBnsdX1RRRbXixQ/preview"
           }
         ]
       }
